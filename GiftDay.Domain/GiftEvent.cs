3619d6b124454f076999e12de78fdbf820b2958f
Nibbles <me@nibbledbit.co.uk>
2022-06-02 10:21:26 +0100

    added projs and first domain

---
+++ GiftDay.Domain/GiftEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace GiftDay.Domain
+{
+    public class GiftEvent
+    {
+        public GiftEvent(DateTime eventDate, string eventTitle)
+        {
+            EventDate = eventDate;
+            EventTitle = eventTitle;
+        }
+
+        public DateTime EventDate { get; set; }
+        public string EventTitle { get; set; }
+
+
+    }
+}

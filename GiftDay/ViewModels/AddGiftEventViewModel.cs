124c5d24b5f6a37cbeccd6964a8c678b1c6e7557
Nibbles <me@nibbledbit.co.uk>
2022-07-28 08:57:35 +0100

    Building out add custom event page

--- GiftDay/ViewModels/AddGiftEventViewModel.cs
+++ GiftDay/ViewModels/AddGiftEventViewModel.cs
@@ -6,30 +6,34 @@ using System.Diagnostics.Metrics;
 using System.Windows.Input;
 using GiftDay.Domain;
 using GiftDay.Common;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace GiftDay.ViewModels;
 
 public partial class AddGiftEventViewModel : ViewModelBase {
     private readonly IEventsService events;
 
-    [ObservableProperty]
-    List<Domain.EventType> types = new List<EventType>() { EventType.Anniversary, EventType.Birthday, EventType.Custom };
+    //put enums on a page
+    //[ObservableProperty]
+    //List<Domain.EventType> types = new List<EventType>() { EventType.Anniversary, EventType.Birthday, EventType.Custom };
 
     [ObservableProperty]
     string title;
 
     [ObservableProperty]
-    EventType type;
-
-    [ObservableProperty]
     DateTime eventDate;
 
     public AddGiftEventViewModel(INavigationService navigationService, IEventsService events) : base(navigationService) {
+
         this.events = events;
+
+        eventDate = DateTime.Today;
     }
+
 
     [RelayCommand]
     void Create() {
-        events.CreateEvent(title, type, eventDate);
+        events.CreateEvent(title, EventType.Custom, eventDate);
     }
 }
